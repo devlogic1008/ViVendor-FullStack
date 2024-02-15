f59494be57115e74d5e35bd3be647a2bb6a23baa
Muhammad Usman <usmanmaliksahib2611@gmail.com>
2024-02-15 15:11:22 +0500

    commit

--- src/routes/v1/auth.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerUser} = require('../../controllers/auth.controllers');
-
-// Register route
-router.post('/register', registerUser);
-
-module.exports = router;
